5745eee190286e166d854f0acdd4e1157cfacc03
mikegee <gee.24@osu.edu>
2012-03-09 22:00:24 -0500

    fresh rails app

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+
+gem 'sqlite3'
+
+gem 'json'
+gem 'jquery-rails'
+
+group :development do
+  gem 'capistrano'
+end
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RackShibTestApp::Application.config.secret_token = '5cf6616c92466b8d6879ec9c9446ac1b3e94f65b0535446d3da995dbaa2d87950c59e71108bc3dafad65e323fd4b0cc76cd9080735c78434df6b484e398a704a'
